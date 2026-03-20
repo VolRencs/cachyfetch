92d6ae992e55858cde890fba4a806f0f6908069f
VolRen <86468666+VolRencs@users.noreply.github.com>
2026-03-20 08:54:18 +0300

    aaa

--- src/fetch.zig
+++ src/fetch.zig
@@ -451,9 +451,12 @@ fn wlSend(sock: std.net.Stream, obj: u32, op: u16, body: []const u8) !void {
     std.mem.writeInt(u32, hdr[0..4], obj, .little);
     std.mem.writeInt(u32, hdr[4..8], (sz << 16) | op, .little);
 
-    var writer = sock.writer();
+    var wbuf: [4096]u8 = undefined;
+    var writer = sock.writer(&wbuf); // передаём буфер
+
     try writer.writeAll(std.mem.asBytes(&hdr));
     try writer.writeAll(body);
+    try writer.flush();
 }
 
 fn wlGet32(b: []const u8, off: *usize) u32 {
