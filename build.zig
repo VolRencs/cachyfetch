8d54aac16a07b7d665c492dc9022ac7f7ca2fc62
VolRen <86468666+VolRencs@users.noreply.github.com>
2026-03-20 05:03:02 +0300

    Test

--- build.zig
+++ build.zig
@@ -4,20 +4,27 @@ pub fn build(b: *std.Build) void {
     const target   = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name     = "cachyfetch",
-        .target   = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("fetch", b.createModule(.{
+    const fetch_mod = b.createModule(.{
         .root_source_file = b.path("src/fetch.zig"),
-    }));
-    exe.root_module.root_source_file = b.path("src/main.zig");
+        .target           = target,
+        .optimize         = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name        = "cachyfetch",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target           = target,
+            .optimize         = optimize,
+            .imports          = &.{
+                .{ .name = "fetch", .module = fetch_mod },
+            },
+        }),
+    });
     exe.root_module.strip = optimize != .Debug;
     b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd  = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-    const run_step = b.step("run", "Run cachyfetch");
-    run_step.dependOn(&run_cmd.step);
+    b.step("run", "Run cachyfetch").dependOn(&run_cmd.step);
 }
