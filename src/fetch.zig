eaf464311c688ef3b7ba0162e3ce69847cfadb1e
VolRen <86468666+VolRencs@users.noreply.github.com>
2026-03-20 07:52:05 +0300

    Test

--- src/fetch.zig
+++ src/fetch.zig
@@ -454,8 +454,8 @@ fn wlSend(sock: std.net.Stream, obj: u32, op: u16, body: []const u8) void {
     var wbuf: [4096]u8 = undefined;
     var writer = sock.writer(&wbuf);
 
-    writer.writeBytes(&hdr) catch {};
-    writer.writeBytes(body) catch {};
+    writer.write(&hdr) catch {};
+    writer.write(body) catch {};
 }
 
 fn wlGet32(b: []const u8, off: *usize) u32 {
@@ -528,7 +528,7 @@ fn waylandMonitors(a: A) [][]const u8 {
     done: while (true) {
         var hdr: [8]u8 = undefined;
 
-        const hn = reader.readBytes(&hdr) catch break;
+        const hn = reader.read(&hdr) catch break;
         if (hn != 8) break;
 
         const sender = std.mem.readInt(u32, hdr[0..4], .little);
@@ -544,7 +544,7 @@ fn waylandMonitors(a: A) [][]const u8 {
         const body = rbuf[0..bsz];
 
         if (bsz > 0) {
-            const bn = reader.readBytes(body) catch break;
+            const bn = reader.read(body) catch break;
             if (bn != bsz) break;
         }
 
